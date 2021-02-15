e2823e0ab64e84ed96c09ffed9f7d269d919f828
BriggsFrazer <BriggsFrazer@users.noreply.github.com>
2021-02-15 17:14:00 +0000

    Added Item data + framework for item panels

---
+++ Brackeys2021 Game Jam submit/Brackeys2021/Assets/Scripts/ItemData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ItemData", menuName = "Item Data", order = 51)]
+public class ItemData : ScriptableObject
+{
+    [SerializeField]
+    public string itemName;
+
+
+    [SerializeField]
+    public string description;
+
+
+    [SerializeField]
+    public Sprite bigSprite;
+
+
+    [SerializeField]
+    public Sprite littleSprite;
+
+
+    [SerializeField]
+    public bool wearable;
+
+
+
+}
+
---
+++ Brackeys2021 Game Jam submit/Brackeys2021/Assets/Scripts/RewardItem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+
+public class RewardItem : MonoBehaviour
+{
+    [SerializeField]
+    private ItemData itemData;
+
+    private GameEvent OnRewardSelected;
+
+
+    private TMP_Text itemName;
+
+
+    private Text description;
+
+
+    private Image icon;
+
+
+    private void initData()
+    {
+        //itemName.text = itemData.itemName;
+        /*this.gameObject.transform.Find("Contents").gameObject.transform.Find("TitleText").GetComponent<TextMeshPro>().SetText(itemData.itemName);*/
+       //Debug.Log(this.gameObject.name);
+    }
+
+    private void Start()
+    {
+        this.gameObject.transform.Find("Contents").gameObject.transform.Find("TitleText").GetComponent<TextMeshPro>().SetText(itemData.itemName);
+        initData();
+    }
+    private void OnMouseDown()
+    {
+        initData();
+        Debug.Log("Pressed!");
+        OnRewardSelected.Raise();
+    }
+
+    private void DrawText()
+    {
+
+    }
+}
