b1aeb12c847d5f8e3ffb140c0d291abccc980f4f
BriggsFrazer <BriggsFrazer@users.noreply.github.com>
2021-02-17 15:36:35 +0000

    Added Health Bar Numbers

---
+++ Brackeys2021 Game Jam submit/Brackeys2021/Assets/Scripts/PlayerHealthNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthNumbers : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var PlayerHealthMax = (float)GameObject.Find("Player").GetComponent<PlayerData>().PlayerMaxHealth;
+        var PlayerHealthCurrent = (float)GameObject.Find("Player").GetComponent<PlayerData>().PlayerCurrentHealth;
+
+        this.GetComponent<UnityEngine.UI.Text>().text = (PlayerHealthCurrent + "/" + PlayerHealthMax);
+    }
+}
