aa1d49da714bca2198d07db5ef522d2b9d13a901
BriggsFrazer <BriggsFrazer@users.noreply.github.com>
2021-02-18 15:13:16 +0000

    Item cards complete

--- Brackeys2021 Game Jam submit/Brackeys2021/Assets/Scripts/RewardItem.cs
+++ Brackeys2021 Game Jam submit/Brackeys2021/Assets/Scripts/RewardItem.cs
@@ -26,21 +26,29 @@ public class RewardItem : MonoBehaviour
 
     public int PanelID;
 
+    private Color[] colors = { new Color(0.1725f, 1, 0.98f), new Color(0.227f, 1, 0.6f), new Color(1, 0.741f, 0), new Color(1, 0.584f, 0.871f),  Color.green }; 
+
     private void initData()
     {
         itemData = GameObject.Find("Player").GetComponent<ItemTracker>().chosenItems[PanelID].GetComponent<Item>().itemData;
 
-        Debug.Log(itemData.name);
+        
 
-        this.gameObject.transform.Find("Card").gameObject.transform.Find("TitleText").GetComponent<UnityEngine.UI.Text>().text = (itemData.name);
+        this.gameObject.transform.Find("Card").gameObject.transform.Find("TitleText").GetComponent<UnityEngine.UI.Text>().text = (itemData.itemName);
+        
+        this.gameObject.transform.Find("Card").gameObject.transform.Find("ItemIcon").GetComponent<UnityEngine.UI.Image>().sprite = (itemData.bigSprite);
+        
+        this.gameObject.transform.Find("Card").gameObject.transform.Find("Border").GetComponent<UnityEngine.UI.Image>().color = colors[itemData.demographicID];
+
+        this.gameObject.transform.Find("Card").gameObject.transform.Find("Description").GetComponent<UnityEngine.UI.Text>().text = itemData.description;
 
         Debug.Log("Pressed!");
     }
 
-    private void Start()
+    private void OnStart()
     {
         PanelID = 0;
-
+        initData();
     }
     public void OnClick()
     {
