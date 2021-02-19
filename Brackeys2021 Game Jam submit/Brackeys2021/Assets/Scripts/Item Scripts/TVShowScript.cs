f44ecbc296c30899b556127cc0b5654f276a9505
BriggsFrazer <BriggsFrazer@users.noreply.github.com>
2021-02-19 10:16:49 +0000

    Added card sprites at the bottom of screen

---
+++ Brackeys2021 Game Jam submit/Brackeys2021/Assets/Scripts/Item Scripts/TVShowScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVShowScript : MonoBehaviour, ItemEffect
+{
+    public void AddEffect()
+    {
+        Debug.Log("TV Show used");
+        GameObject.Find("Player").GetComponent<PlayerData>().DemographicNumbers[0] += 10;
+    }
+
+    public void RemoveEffect()
+    {
+
+    }
+
+    public void PassiveCombatEffect()
+    {
+
+    }
+
+    public void PassiveTurnEffect()
+    {
+
+    }
+}
