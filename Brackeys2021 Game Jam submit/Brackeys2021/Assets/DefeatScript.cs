13a047ece3d8978b1a2556e6a14054cd0728802b
BriggsFrazer <BriggsFrazer@users.noreply.github.com>
2021-02-20 17:58:03 +0000

    Added end screens

---
+++ Brackeys2021 Game Jam submit/Brackeys2021/Assets/DefeatScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefeatScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<UnityEngine.UI.Text>().text = "YOU HAVE BEEN DEFEATED! \n YOU HAVE REACHED ROUND: " + GameObject.Find("Player").GetComponent<PlayerData>().currentStage + "\n BACK TO MENU";
+    }
+}
---
+++ Brackeys2021 Game Jam submit/Brackeys2021/Assets/WinScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<UnityEngine.UI.Text>().text = "CONGRATULATIONS YOU HAVE WON! \n YOU GAINED ENOUGH FAN POWER TO BE UNSTOPPABLE \n WE WERE TRULY STRONGER TOGETHER!";
+    }
+}
