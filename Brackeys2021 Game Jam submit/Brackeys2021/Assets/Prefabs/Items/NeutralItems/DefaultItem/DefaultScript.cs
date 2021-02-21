a5152779e58af50653983286a44c063b6ec80c19
BriggsFrazer <BriggsFrazer@users.noreply.github.com>
2021-02-21 00:08:28 +0000

    Final balance fix

--- Brackeys2021 Game Jam submit/Brackeys2021/Assets/Prefabs/Items/NeutralItems/DefaultItem/DefaultScript.cs
+++ Brackeys2021 Game Jam submit/Brackeys2021/Assets/Prefabs/Items/NeutralItems/DefaultItem/DefaultScript.cs
@@ -7,7 +7,8 @@ public class DefaultScript : MonoBehaviour, ItemEffect
     public void AddEffect()
     {
 
-        GameObject.Find("Player").GetComponent<PlayerData>().PlayerCurrentHealth += (GameObject.Find("Player").GetComponent<PlayerData>().PlayerCurrentHealth / 10) * 2;
+        GameObject.Find("Player").GetComponent<PlayerData>().PlayerCurrentHealth += 100;
+
         if (GameObject.Find("Player").GetComponent<PlayerData>().PlayerCurrentHealth > GameObject.Find("Player").GetComponent<PlayerData>().PlayerMaxHealth)
         {
             GameObject.Find("Player").GetComponent<PlayerData>().PlayerCurrentHealth = GameObject.Find("Player").GetComponent<PlayerData>().PlayerMaxHealth;
