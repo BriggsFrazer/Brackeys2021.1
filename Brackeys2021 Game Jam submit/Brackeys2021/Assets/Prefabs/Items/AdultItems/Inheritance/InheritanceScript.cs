b5ea4e5f2f6a57b94ec4f9b13a1a21b44c12bc00
BriggsFrazer <BriggsFrazer@users.noreply.github.com>
2021-02-21 00:04:19 +0000

    Finished all items and bugfixed

--- Brackeys2021 Game Jam submit/Brackeys2021/Assets/Prefabs/Items/AdultItems/Inheritance/InheritanceScript.cs
+++ Brackeys2021 Game Jam submit/Brackeys2021/Assets/Prefabs/Items/AdultItems/Inheritance/InheritanceScript.cs
@@ -7,7 +7,7 @@ public class InheritanceScript : MonoBehaviour, ItemEffect
     public void AddEffect()
     {
         var count = (int)GameObject.Find("Player").GetComponent<PlayerData>().DemographicNumbers[3]/2;
-        GameObject.Find("Player").GetComponent<PlayerData>().DemographicNumbers[2] = count;
+        GameObject.Find("Player").GetComponent<PlayerData>().DemographicNumbers[3] -= count;
         GameObject.Find("Player").GetComponent<PlayerData>().Money += count*50;
 
     }
